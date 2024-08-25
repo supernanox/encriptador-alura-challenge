72a70781a432f3aec1eaeb915ed5a300c65ccff3
supernanox <supernanox@gmail.com>
2024-08-24 22:34:31 -0300

    Continua modificación de Desencriptar

--- aplication.js
+++ aplication.js
@@ -25,12 +25,19 @@ function validarParaEncriptar(texto) {
 
 function validarParaDesencriptar(texto) {
     if (texto.length > 0) {
-        
+        for (i = 0; i < texto.length - 1; i++) {
+            if (!CARACTERES_ACEPTADOS.includes(i)) {
+                alert("Caracter inválido: " + carActual);
+                break;
+            } else {
+
+            }            
+        }
     } else {
         alert("Debe ingresar el texto a desencriptar.");
     }
 
-    mostrarResultado(egreso);
+    desencriptar(texto);
     resetearEgreso();
 
 }
@@ -40,19 +47,19 @@ function btnEncriptar() {
     validarParaEncriptar(ingreso);
 }
 
-function btnDesencriptar(){
+function btnDesencriptar() {
     ingreso = document.getElementById("texto-procesar").value;
-    validarParaEncriptar(ingreso);
+    validarParaDesencriptar(ingreso);
 }
 
 function agregarParaEncriptar(caracter) {
-    switch(caracter){
+    switch (caracter) {
         case "a":
             egreso = egreso + "ai";
             break;
         case "e":
             egreso = egreso + "enter";
-            break;        
+            break;
         case "i":
             egreso = egreso + "imes";
             break;
@@ -68,38 +75,23 @@ function agregarParaEncriptar(caracter) {
     }
 }
 
-function agregarParaDesencriptar(caracter) {
-    switch(caracter){
-        case "a":
-            egreso = egreso + "ai";
-            break;
-        case "e":
-            egreso = egreso + "enter";
-            break;        
-        case "i":
-            egreso = egreso + "imes";
-            break;
-        case "o":
-            egreso = egreso + "ober";
-            break;
-        case "u":
-            egreso = egreso + "ufat";
-            break;
-        default:
-            egreso = egreso + caracter;
-            break;
+function desencriptar(textoParaDesencriptar) {
+    let egresoDesencriptado = "";
+    if (textoParaDesencriptar.includes("ai")) {
+        egresoDesencriptado = textoParaDesencriptar.replaceAll("ai", "a");
     }
+    mostrarResultado();
 }
 
-function mostrarResultado(resultado){
-let texto = document.getElementById("texto-resultado");
-let elementosparaQuitar = document.querySelectorAll(".elementos-para-quitar");
+function mostrarResultado() {
+    let texto = document.getElementById("texto-resultado");
+    let elementosparaQuitar = document.querySelectorAll(".elementos-para-quitar");
 
-texto.removeAttribute("hidden");
-texto.innerHTML = egreso;
-elementosparaQuitar.forEach(elemento => {elemento.setAttribute("hidden", "")});
+    texto.removeAttribute("hidden");
+    texto.innerHTML = egreso;
+    elementosparaQuitar.forEach(elemento => { elemento.setAttribute("hidden", "") });
 }
 
-function resetearEgreso(){
+function resetearEgreso() {
     egreso = "";
 }
